8d2393c29a34ab1c7730f1af7a129d9a4f483c7d
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-04-11 13:12:15 +0900

    add LD_LIBRARY_PATH To test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 source "$HOME/.cargo/env"
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+
+# for macos
 export DYLD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$DYLD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH="`pwd`/rust-lightning/target/debug:$DYLD_LIBRARY_PATH"
+
+# for linux
+export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="`pwd`/rust-lightning/target/debug:$LD_LIBRARY_PATH"
 
 cd rust-lightning/bindings && cargo build --features "debug_assertions"
 cd ../..
