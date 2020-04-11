5476fd0187bf43c42bde899bb8bf481ca82cb173
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-04-11 15:32:59 +0900

    stop using test.sh for testing

--- test.sh
+++
@@ -1,6 +0,0 @@
-. "$HOME/.cargo/env"
-export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-
-cd rust-lightning/bindings && cargo build --features "debug_assertions"
-cd ../..
-dotnet test tests/NRustLightning.Tests
