b2de103b4c015df812acf73311d4165066b948d0
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-04-11 15:10:39 +0900

    move projects under src/ and tests/

--- test.sh
+++ test.sh
@@ -1,14 +1,6 @@
-source "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
 export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-
-# for macos
-export DYLD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$DYLD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="`pwd`/rust-lightning/target/debug:$DYLD_LIBRARY_PATH"
-
-# for linux
-export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="`pwd`/rust-lightning/target/debug:$LD_LIBRARY_PATH"
 
 cd rust-lightning/bindings && cargo build --features "debug_assertions"
 cd ../..
-dotnet test NRustLightning.Tests
+dotnet test tests/NRustLightning.Tests
