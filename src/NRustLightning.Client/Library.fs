88ed508727f23e3e9450aa9037e0ad24c37ba72f
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-05-07 01:05:21 +0900

    add NRustlightning.Client as a pacakge

--- src/NRustLightning.Client/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace NRustLightning.Client
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/NRustLightning.Client/NRustLightningClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using NRustLightning.Server.Models;
+using NRustLightning.Server.Models.Response;
+
+namespace NRustLightning.Client
+{
+    public class NRustLightningClient : IDisposable
+    {
+        private HttpClient _client;
+        private Uri _baseUri;
+
+        public NRustLightningClient(string baseUrl)
+        {
+            _client = new HttpClient(new HttpClientHandler());
+            _baseUri = new Uri(baseUrl);
+        }
+        
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        public async Task<NodeInfo> GetInfo()
+        {
+            var resp = await _client.GetAsync(new Uri(_baseUri, "/api/v1/info")).ConfigureAwait(false);
+            resp.EnsureSuccessStatusCode();
+            var content = await resp.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<NodeInfo>(content);
+        }
+    }
+}
