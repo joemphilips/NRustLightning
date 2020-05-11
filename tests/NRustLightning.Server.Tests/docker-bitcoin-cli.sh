d17e324857166e8f37102098d4f12cb3c911061f
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-05-11 20:24:43 +0900

    add scripts to run clis in docker-compose

--- tests/NRustLightning.Server.Tests/docker-bitcoin-cli.sh
+++ tests/NRustLightning.Server.Tests/docker-bitcoin-cli.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-bitcoind_container_id="$(docker ps -q --filter label=com.docker.compose.project=nrustlightningservertests --filter label=com.docker.compose.service=bitcoind)"
-docker exec -ti "$bitcoind_container_id" bitcoin-cli -datadir="/data" -rpcport=${BITCOIND_RPC_PORT} -rpcpassword=${BITCOIND_RPC_PASS} -rpcuser=${BITCOIND_RPC_USER} "$@"
+docker-compose exec bitcoind bitcoin-cli -datadir="/data" -rpcport=${BITCOIND_RPC_PORT} -rpcpassword=${BITCOIND_RPC_PASS} -rpcuser=${BITCOIND_RPC_USER} "$@"
