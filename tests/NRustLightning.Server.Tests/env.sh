38bdb31ec5fe0081090895f3297a92cca423dca9
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-06-18 19:08:50 +0900

    add env.sh for test

---
+++ tests/NRustLightning.Server.Tests/env.sh
@@ -0,0 +1,10 @@
+export BITCOIND_RPC_AUTH='johndoe:a488d176bdfcf0fa831c8ef6f5b5fa71$a39a53f2bb8e5dce7a54b4df2df9787a9ab5b02db2fcc2ad83ef954bd883de23'
+export BITCOIND_RPC_USER=johndoe
+export BITCOIND_RPC_PASS='HmcwPBb0KRWFWlSXkSY_DI70JekYyAr5CPx2N2i8RjA='
+export BITCOIND_RPC_PORT=12120
+export LND_REST_PORT=10095
+export LIGHTNINGD_RPC_PORT=10509
+export HTTP_PORT=10320
+export NBXPLORER_PORT=12940
+export DATA_PATH=`pwd`/bin/Debug/netcoreapp3.1/tmp
+export LIGHTNINGD_VOLUME="lightningd_dir"
