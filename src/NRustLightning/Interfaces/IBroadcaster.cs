cc80d5ed22e6cc75bff35f89ec2ad8d9201d40e9
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-06-26 16:27:22 +0900

    WIP: refactoring to use Option instead of Ref for fn pointer

--- src/NRustLightning/Interfaces/IBroadcaster.cs
+++ src/NRustLightning/Interfaces/IBroadcaster.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Runtime.InteropServices;
+using NBitcoin;
 using NRustLightning.Adaptors;
+using Network = NBitcoin.Network;
 
 namespace NRustLightning.Interfaces
 {
@@ -11,9 +14,30 @@ namespace NRustLightning.Interfaces
     /// keep in mind that the static delegate defined in this class may call the
     /// delegate from multiple threads at the same time, so it must make thread safe,
     /// If you want to hold mutable state in it.
+    ///
+    /// You usually want to inherit from abstract class <see cref="Broadcaster"/> instead of this.
+    /// Use only when the class wants to inherit from other abstract class and thus you have to use the interface.
     /// </summary>
     public interface IBroadcaster
     {
-        ref BroadcastTransaction BroadcastTransaction { get; }
+        BroadcastTransaction BroadcastTransaction { get; }
+    }
+
+    public abstract class Broadcaster : IBroadcaster
+    {
+        private readonly Network _n;
+        public Broadcaster(NBitcoin.Network n)
+        {
+            _n = n;
+        }
+        public abstract void BroadcastTransactionImpl(Transaction tx);
+
+        public virtual void BroadcastTransactionCore(ref FFITransaction ffiTx)
+        {
+            var tx = ffiTx.AsTransaction(_n);
+            BroadcastTransactionImpl(tx);
+        }
+
+        public BroadcastTransaction BroadcastTransaction => this.BroadcastTransactionCore;
     }
 }
--- src/NRustLightning/Interfaces/IChainWatchInterface.cs
+++ src/NRustLightning/Interfaces/IChainWatchInterface.cs
@@ -14,23 +14,34 @@ namespace NRustLightning.Interfaces
         /// <summary>
         /// Provides a txid/random-scriptPubKey-in-the-tx which must be watched for.
         /// </summary>
-        ref InstallWatchTx InstallWatchTx { get; }
+        InstallWatchTx InstallWatchTx { get; }
         /// <summary>
         /// Provides an outpoint which must be watched for, providing any transactions which spend the given outpoint.
         /// </summary>
-        ref InstallWatchOutPoint InstallWatchOutPoint { get; }
+        InstallWatchOutPoint InstallWatchOutPoint { get; }
         /// <summary>
         /// Indicates that a listener needs to see all transactions.
         /// </summary>
-        ref WatchAllTxn WatchAllTxn { get; }
+        WatchAllTxn WatchAllTxn { get; }
         /// <summary>
         /// Gets the script and value in satoshis for a given unspent transaction output given a short_channel_id
         /// (a.k.a. unspent_tx_output_identifier). For BTC/tBTC channels the top three
         /// bytes are the block height, the next 3 the transaction index within the block and the
         /// final two the output within the tx.
         /// </summary>
-        ref GetChainUtxo GetChainUtxo { get; }
+        GetChainUtxo GetChainUtxo { get; }
         
-        ref FilterBlock FilterBlock { get; }
+        FilterBlock FilterBlock { get; }
+    }
+
+    public abstract class ChainWatchInterface : IChainWatchInterface
+    {
+        
+        public 
+        public InstallWatchTx InstallWatchTx => this.InstallWatchTxCore;
+        public InstallWatchOutPoint InstallWatchOutPoint { get; }
+        public WatchAllTxn WatchAllTxn { get; }
+        public GetChainUtxo GetChainUtxo { get; }
+        public FilterBlock FilterBlock { get; }
     }
 }
